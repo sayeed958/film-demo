d5a221cc586aa5adaed5f377e3bb36a88f6f6afc
sayeed958 <md.sayeed958@gmail.com>
2020-10-24 15:37:23 +0400

    minor change

--- sequelize-migrations/seeders/seedModels.js
+++ sequelize-migrations/seeders/seedModels.js
@@ -1,6 +1,8 @@
 
 const {
     Films,
+	Users,
+	Comments
 } = require('./seeder-data');
 
 module.exports = {
@@ -10,6 +12,8 @@ module.exports = {
 	up: async (queryInterface) => {
 		// eslint-disable-next-line implicit-arrow-linebreak
 		await queryInterface.bulkInsert({ tableName: 'Films', schema: 'public' }, Films);
+		await queryInterface.bulkInsert({ tableName: 'Users', schema: 'public' }, Users);
+		await queryInterface.bulkInsert({ tableName: 'Comments', schema: 'public' }, Comments);
 	},
 
 	// eslint-disable-next-line no-unused-vars
--- sequelize-migrations/seeders/seeder-data.js
+++ sequelize-migrations/seeders/seeder-data.js
@@ -1,9 +1,13 @@
 /* eslint-disable no-unused-vars */
+
 const uuidv4 = require('uuid/v4');
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
+const defaultPassword = bcrypt.hashSync('TEST@123', salt);
 /**
  * @example ddlg
-*This file insert the data into Films
-*/
+ *This file insert the data into Films
+ */
 module.exports = {
     Films: [
         {
@@ -39,6 +43,28 @@ module.exports = {
             ticketPrice: 200,
             country: 'USA',
             genre: ['drama', 'action'],
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+    ],
+    Users: [
+        {
+            uuid: uuidv4(),
+            firstName: 'Mohammad',
+            lastName: 'Sayeed',
+            email: 'md@gmail.com',
+            phoneNo: '0526081006',
+            password: defaultPassword,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+    ],
+    Comments: [
+        {
+            uuid: uuidv4(),
+            userId: 1,
+            filmId: 1,
+            comment: 'awesome movie',
             createdAt: new Date(),
             updatedAt: new Date()
         },
